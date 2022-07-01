c64828d82e7ba7edf479be369fada8294554aae7
Adefiola <adefiolakolajo@gmail.com>
2022-06-30 17:19:15 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  *main - Entry point
  *Return: Always 0 (success)
  */
- int main(void)
- {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
- }
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
