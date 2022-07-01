fa054d388a8ca4f674c9fa4266477e727af6ea00
Adefiola <adefiolakolajo@gmail.com>
2022-07-01 14:16:06 -0700

    task1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include<stdlib.h>
+
+#include<time.h>
+
+#include<stdio.h>
+
+
+/**
+ *main-checkifarandomnumberispositiveornegative
+ *Return:0iferror
+ */
+
+intmain(void)
+  
+{  
+intn;
+intnLast;
+srand(time(0));
+n=rand()-RAND_MAX/2;
+nLast=n%10;
+if(nLast>5)
+printf("Lastdigitof%dis%dandisgreaterthan5\n",n,nLast);
+elseif(nLast<6&&nLast!=0)
+printf("Lastdigitof%dis%dandislessthan6andnot0\n",n,nLast);
+else
+printf("Lastdigitof%dis%dandis0\n",n,nLast);
+return(0);
+}
