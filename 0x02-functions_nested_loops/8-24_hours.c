33d0cb2d027e4c20e508abba7527ab7e00ad01ad
Adefiola <adefiolakolajo@gmail.com>
2022-07-05 20:26:55 -0700

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ for (j = 0; j < 60; j++)
 if (i < 10)
 {
 _putchar('0');
-_putchar(i + '0')
+_putchar(i + '0');
 }
 else if (i >= 10)
 {
@@ -31,9 +31,7 @@ else if (j > 10)
 {
 _putchar(':');
 _putchar((j / 10 + '0');
-_putchar((j % 10) + '0')
+_putchar((j % 10) + '0');
 }
-_putchar(i);
-_putchar(':')
-_putchar(j)
+_putchar('\n');
 }
