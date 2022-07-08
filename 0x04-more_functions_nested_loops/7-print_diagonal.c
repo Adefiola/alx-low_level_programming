d3943c9fddd9c47c527d88a62c31d76f1c6a3beb
Adefiola <adefiolakolajo@gmail.com>
2022-07-07 23:52:03 -0700

    task7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,16 +8,16 @@
 void print_diagonal(int n)
 {
 int len, space;
-if (n > 0)
+if (n <= 0)
+_putchar('\n');
+else
+{
 for (len = 0; len < n; len++)
 {
 for (space = 0; space < len; space++)
 _putchar(' ');
-_putchar('\\');
-if (len == n - 1)
-continue;
+_putchar(92);
 _putchar('\n');
 }
 }
-_putchar('\n');
 }
