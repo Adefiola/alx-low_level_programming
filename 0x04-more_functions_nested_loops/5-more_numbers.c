5e9967089842b708b55d059deb32a160f6a6681a
Adefiola <adefiolakolajo@gmail.com>
2022-07-07 23:40:03 -0700

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,16 +8,13 @@
 
 void more_numbers(void)
 {
-int i;
-int b;
-for (i = 0; i <= 9; i++)
+  int i, b;
+for (i = 0; i < 10; i++)
 {
-for (b = 0; b <= 14; b++)
+for (b = 0; b < 15; b++)
 {
-if (b > 9)
-{
-_putchar('1');
-}
+if (b >= 10)
+_putchar((b / 10) + '0');
 _putchar((b % 10) + '0');
 }
 _putchar('\n');
